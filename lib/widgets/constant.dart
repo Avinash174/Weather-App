e596e252af37d65e2394300b4810e81dd029caab
Avinash174 <avinashmagar15@gmail.com>
2024-05-21 11:17:38 +0530

    new changes add

--- lib/widgets/constant.dart
+++ lib/widgets/constant.dart
@@ -6,4 +6,22 @@ class Constant {
   final tertiaryColor = const Color(0xff205cf1);
   final blackColor = const Color(0xff000000);
   final greyColor = const Color(0x00ff9adb);
+  final LinearGradientBlue = const LinearGradient(
+    end: Alignment.bottomCenter,
+    begin: Alignment.topRight,
+    colors: [
+      Color(0xffABcff2),
+      Color.fromARGB(255, 101, 156, 211),
+    ],
+    stops: [0.0, 1.0],
+  );
+  final LinearGradientPurple = const LinearGradient(
+    end: Alignment.bottomRight,
+    begin: Alignment.topLeft,
+    colors: [
+      Color(0xff51087e),
+      Color.fromARGB(255, 120, 1, 195),
+    ],
+    stops: [0.0, 1.0],
+  );
 }
