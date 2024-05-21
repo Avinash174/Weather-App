0beffd25c3ca87da16de7e6e548f8a01137354bc
Avinash174 <avinashmagar15@gmail.com>
2024-05-21 10:43:54 +0530

    new changes done

--- lib/widgets/constant.dart
+++ lib/widgets/constant.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Constant {
-  final primaryColor = Color.fromARGB(255, 134, 107, 252);
-  final secondaryColor = Color(0xffa1c6fd);
-  final tertiaryColor = Color(0xff205cf1);
-  final blackColor = Color(0xff000000);
+  final primaryColor = const Color.fromARGB(255, 134, 107, 252);
+  final secondaryColor = const Color(0xffa1c6fd);
+  final tertiaryColor = const Color(0xff205cf1);
+  final blackColor = const Color(0xff000000);
 
-  final greyColor = Color(0xff9adb);
+  final greyColor = const Color(0x00ff9adb);
 }
